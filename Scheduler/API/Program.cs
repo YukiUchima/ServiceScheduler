57ed6139d7a010e8edb3f17564928cf3fbc68552
Yuki Uchima <81452155+YukiUchima@users.noreply.github.com>
2025-02-03 20:33:24 -0700

    Setup SQL Server

--- Scheduler/API/Program.cs
+++ Scheduler/API/Program.cs
@@ -4,11 +4,11 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer(); //swagger
-//builder.Services.AddSwaggerGen(); //swagger
+builder.Services.AddSwaggerGen(); //swagger
 builder.Services.AddControllers();
 
 //NEW 
-builder.Services.AddDbContext<ServiceSchedulerContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ServiceSchedulerContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("LocalhostConnection")));
 
 var app = builder.Build();
 
